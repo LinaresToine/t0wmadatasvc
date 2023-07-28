b2df28eea30b81a2658d09fe3f54f06e4f031319
Antonio <a.linares@cern.ch>
2023-07-28 12:49:30 -0500

    fix query

--- src/python/DataPrimaryDatasetConfig.py
+++ src/python/DataPrimaryDatasetConfig.py
@@ -20,24 +20,20 @@ class PrimaryDatasetConfig(RESTEntity):
     :returns: PrimaryDataset, Acquisition era, minimum run, maximum run, CMSSW, PhysicsSkim, DqmSeq, GlobalTag"""
 
     sql = """
-            SELECT reco_config.primds primds, run_config.acq_era acq_era, MIN(run_config.run) min_run, MAX(run_config.run) max_run, reco_config.cmssw cmssw, reco_config.global_tag global_tag, reco_config.physics_skim physics_skim, reco_config.dqm_seq dqm_seq
-            FROM reco_config
-            JOIN run_config ON run_config.run = reco_config.run
-
+            SELECT primds, acq_era, min_run, max_run, cmssw, global_tag, physics_skim, dqm_seq
+            FROM primary_dataset_config
             """
     sql_with_primds = "WHERE primds = :primary_dataset"
-    sql_group = "GROUP BY run_config.acq_era, reco_config.primds, reco_config.cmssw,  reco_config.global_tag, reco_config.physics_skim, reco_config.dqm_seq"
-    sql_order = "ORDER BY primds, min_run desc"
+    sql_order_with_primds = "ORDER BY min_run desc"
+    sql_order_without_primds = "ORDER BY primds, min_run desc"
 
 
     if primary_dataset is not None:
         sql += sql_with_primds
-        sql += sql_group
-        sql += sql_order
+        sql += sql_order_with_primds
         c, _ = self.api.execute(sql, primds = primary_dataset)
     else:
-        sql += sql_group
-        sql += sql_order
+        sql += sql_order_without_primds
         c, _ = self.api.execute(sql, primds = '')
 
     results=c.fetchall()
