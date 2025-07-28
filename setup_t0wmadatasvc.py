cac2bac6df8bc84c17e752dcd529610eb0462829
Antonio Linares <a.linares@cern.ch>
2025-07-28 12:47:59 +0200

    requirements

---
+++ setup_t0wmadatasvc.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+# This setup script is used to build the T0WmaDataSvc pypi package.
+# The version number comes from T0WmaDataSvc/__init__.py and needs to
+# follow PEP 440 conventions
+
+from __future__ import print_function, division
+import os
+import sys
+import importlib
+from setuptools import setup
+from setup_build import list_packages, list_static_files, get_path_to_t0wmadatasvc_root, load_source
+
+# Obnoxiously, there's a dependency cycle when building packages. We'd like
+# to simply get the current T0WmaDataSvc version by using
+# from T0WmaDataSvc import __version__
+# But PYTHONPATH isn't set until after the package is built, so we can't
+# depend on the python module resolution behavior to load the version.
+# Instead, we use the imp module to load the source file directly by
+# filename.
+t0wmadatasvc_root = get_path_to_t0wmadatasvc_root()
+t0wmadatasvc_package = load_source('temp_module', os.path.join(t0wmadatasvc_root,
+                                                             'src',
+                                                             'python',
+                                                             'T0WmaDataSvc',
+                                                             '__init__.py'))
+t0wmadatasvc_version = t0wmadatasvc_package.__version__
+
+# Requirements file for pip dependencies
+requirements = "requirements.txt"
+
+
+def parse_requirements(requirements_file):
+    """
+      Create a list for the 'install_requires' component of the setup function
+      by parsing a requirements file
+    """
+
+    if os.path.exists(requirements_file):
+        # return a list that contains each line of the requirements file
+        return open(requirements_file, 'r').read().splitlines()
+    else:
+        print("ERROR: requirements file " + requirements_file + " not found.")
+        sys.exit(1)
+
+
+setup(name='t0wmadatasvc',
+      version=t0wmadatasvc_version,
+      maintainer='CMS DMWM Group',
+      maintainer_email='cms-tier0-operations@cern.ch',
+      package_dir={'': 'src/python/'},
+      packages=list_packages(['src/python/T0WmaDataSvc']),
+      data_files=list_static_files(),
+      install_requires=parse_requirements(requirements),
+      url="https://github.com/dmwm/t0wmadatasvc",
+      license="Apache License, Version 2.0",
+      )
